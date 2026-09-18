70b968b1b13ceb8f4baf235124f037ba57014521
enzooz <enzosebastiansilvestri@gmail.com>
2026-09-18 19:15:21 -0300

    Agrega 11 canciones por defecto y mejora formato de La bondad de Dios

--- data.js
+++ data.js
@@ -3,18 +3,645 @@ const DEFAULT_SONGS = [
         id: "default-001",
         title: "La bondad de Dios",
         author: "Bethel Music",
-        content: `[Intro] [G] [G4] [G] [G4] [G]
+        content: `
+[Intro] [G] [G4] [G] [G4] [G]
 
-[G]Te amo Dios[C9]   [G]Tu bondad nunca me falla[D/F#] [Em7]Y mis días, en tus manos están[Em7] [C9]Desde el momento que despierto[G]    [D/F#] [Em7]Hasta el anochecer[C9]       [D2]         [G]Yo cantaré de la bondad de Dios
+VERSO 1:
+[G]Te amo Dios
+[C9]Tu bondad [G]nunca me falla
+[D/F#]Y mis [Em7]días, en tus [C9]manos [D]están
+[Em7]Desde el [C9]momento que despierto
+[G]Hasta el [D/F#]anochecer[Em7]
+[C9]Yo cantaré de la [D2]bondad de [G]Dios
 
-[C9]En mi vida has sido bueno[G]   [C9]En mi vida has sido fiel Señor[G]     [D]   [C9]Con cada aliento de mi ser[G]  [D/F#]  [Em7]         [C9]       [D]          [G]Yo cantaré de la bondad de Dios
+CORO:
+[C9]En mi vida has sido [G]bueno
+[C9]En mi vida has sido [G]fiel [D]Señor
+[C9]Con cada aliento de [G]mi [D/F#]ser[Em7]
+[C9]Yo cantaré de la [D]bondad de [G]Dios
 
-[G]Amo Tu voz[C9]                 [G]Me has guiado a través del fuego[D/F#]  [Em7]En la oscuridad[C9]            [D]Tu presencia me acompaña[Em7]  [C9]Te conozco como Padre[G]      [D/F#]  [Em7]Como amigo también[C9]       [D]          [G]Yo cantaré de la bondad de Dios
+VERSO 2:
+[G]Amo Tu voz
+[C9]Me has guiado a [G]través del fuego
+[D/F#]En la oscuri[Em7]dad
+[C9]Tu presencia me [D]acompaña
+[Em7]Te conozco [C9]como Padre
+[G]Como [D/F#]amigo [Em7]también
+[C9]Yo cantaré de la [D]bondad de [G]Dios
 
-[G/B]Tu bondad corre detrás[C9]    [D]            [G]Corre detrás de mí[G/B]               [C9]Tu bondad corre detrás[D]            [G]Corre detrás de mí[G/B]              [C9]Mi vida te doy, yo me rindo a ti[D]           [Em7]Te doy todo de mí[G/B]               [C9]Tu bondad corre detrás[D]            [G]Corre detrás de mí
+CORO:
+[C9]En mi vida has sido [G]bueno
+[C9]En mi vida has sido [G]fiel [D]Señor
+[C9]Con cada aliento de [G]mi [D/F#]ser[Em7]
+[C9]Yo cantaré de la [D]bondad de [G]Dios
 
-[C9]En mi vida has sido bueno[G]   [C9]En mi vida has sido fiel Señor[G]     [D]   [C9]Con cada aliento de mi ser[G]  [D/F#]  [Em7]         [C9]       [D]          [G]Yo cantaré de la bondad de Dios`,
+PUENTE:
+[G/B]Tu bondad [C9]corre detrás
+[D]Corre detrás [G]de mí
+[G/B]Tu bondad [C9]corre detrás
+[D]Corre detrás [G]de mí
+[G/B]Mi vida te [C9]doy, yo me rindo a ti
+[D]Te doy [Em7]todo de mí
+[G/B]Tu bondad [C9]corre detrás
+[D]Corre detrás [G]de mí
+
+[G/B]Tu bondad [C9]corre detrás
+[D]Corre detrás [G]de mí
+[G/B]Tu bondad [C9]corre detrás
+[D]Corre detrás [G]de mí
+[G/B]Mi vida te [C9]doy, yo me rindo a ti
+[D]Te doy [Em7]todo de mí
+[G/B]Tu bondad [C9]corre detrás
+[D]Corre detrás [G]de mí
+
+CORO FINAL:
+[C9]En mi vida has sido [G]bueno
+[C9]En mi vida has sido [G]fiel [D]Señor
+[C9]Con cada aliento de [G]mi [D/F#]ser[Em7]
+[C9]Yo cantaré de la [D]bondad de [G]Dios
+
+[C9]En mi vida has sido [G]bueno
+[C9]En mi vida has sido [G]fiel [D]Señor
+[C9]Con cada aliento de [G]mi [D/F#]ser[Em7]
+[C9]Yo cantaré de la [D]bondad de [Em7]Dios
+[C9]Yo cantaré de la [D]bondad de [G]Dios`,
         order: 1,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-002",
+        title: "Yo soy libre",
+        author: "Marcos Brunet",
+        content: `
+[C]Los ciegos verán por ti
+[C]Los mudos cantarán
+[F]Los cojos saltarán
+[F]Los muertos vivirán
+[A]Las tinieblas huirán
+[G]Por ti yo grito
+[C]Yo soy libre, (Yo soy libre)
+[C]Yo soy libre, (Yo soy libreeee)
+
+CORO:
+[C]Libre para correr
+[C](Libre para correr)
+[F]Libre para danzar
+[F](Libre para danzar)
+[A]Libre para vivir por ti
+[G](Libre para vivir por ti)
+[C]Yo soy libre, (Yo soy libre)
+[C]Yo soy libre, (Yo soy libreee)
+
+PUENTE:
+[C]OoOoOh (OoOoOh)
+[F]OoOoOh (OoOoOh)
+[A]OoOoh  (OoOoh)
+[G]OoOoh  (OoOoh)
+[C]Libre (libre)
+[C]Libre (libreee)
+
+CORO:
+[C]Libre para correr
+[C](Libre para correr)
+[F]Libre para danzar
+[F](Libre para danzar)
+[A]Libre para vivir por ti
+[G](Libre para vivir por ti)
+[C]Yo soy libre, (Yo soy libre)
+[C]Yo soy libre, (Yo soy libreee)`,
+        order: 2,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-003",
+        title: "Hay Libertad",
+        author: "Art Aguilera",
+        content: `[Intro]
+
+[Dm]Hoy puedo danzar con [Bb]libertad
+[F]Porque soy su hijo
+[C]Porque soy su hijo
+
+[Dm]Hoy puedo danzar con [Bb]libertad
+[F]Porque soy amado
+[C]Porque soy amado
+
+[Solo] [Dm] [Bb] [F] [C]
+
+[Dm]Podemos sentir [Bb]Tu gozo
+[Dm]Podemos sentir [Bb]Tu río
+[F]Hay sanidad [C/E]en las aguas
+[Dm]Queremos danzar
+
+[Dm]Podemos sentir [Bb]Tu gozo
+[Dm]Podemos sentir [Bb]Tu río
+[F]Hay sanidad [C/E]en las aguas
+[Dm]Queremos danzar
+
+CORO:
+[Dm]Hay libertad en la [Bb]casa de Dios
+[Dm]Hay libertad en la [Bb]casa de Dios
+[F]Hay libertad, [C]Hay libertad
+
+[Dm]Hay libertad en la [Bb]casa de Dios
+[Dm]Hay libertad en la [Bb]casa de Dios
+[F]Hay libertad, [C]Hay libertad
+
+PUENTE:
+[Dm]Hay li[C/E]ber[F]tad en la [Bb]casa
+[Dm]Hay li[C/E]ber[F]tad al [C]danzar
+
+[Dm]Hay li[C/E]ber[F]tad en la [Bb]casa
+[Dm]Hay li[C/E]ber[F]tad al [C]danzar
+
+[Dm]Hay libertad en la [Bb/D]casa de Dios
+[Dm]Hay libertad en la [Bb]casa de Dios
+[F]Hay libertad, [C]Hay libertad
+
+[Dm]Puedo danzar en la [Bb]casa de Dios
+[Dm]Puedo danzar en la [Bb]casa de Dios
+[F]Puedo danzar [C]y disfrutar
+
+[Dm]Somos libres
+[Bb]Somos libres
+[F]Por su sangre
+[C]Libre soy
+
+[Dm]Somos libres
+[Bb]Somos libres
+[F]Por su sangre
+[C]Libre soy
+[Bb]Libre soy`,
+        order: 3,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-004",
+        title: "Grande y Fuerte",
+        author: "Miel San Marcos",
+        content: `[Intro] [Am] [F7M] [Em7]
+
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+
+CORO:
+[Dm7]Vestido en [G]majestad [Dm7]coronado [Am]con poder
+[F]Digno de [G]toda la [Am]adora[G/B]cion [A7/C#]
+
+[Dm7]Vestido en [G]majestad [Dm7]coronado [Am]con poder
+[F]Toda glo[G]ria y honra sea para Ti
+
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+[Am]Grande y [F7M]fuerte es [Em7]nuestro [Am]Dios
+
+CORO:
+[Dm7]Vestido en [G]majestad [Dm7]coronado [Am]con poder
+[F]Digno de [G]toda la [Am]adora[G/B]cion [A7/C#]
+
+[Dm7]Vestido en [G]majestad [Dm7]coronado [Am]con poder
+[F]Toda glo[G]ria y honra sea para Ti
+
+[Am]Grande
+[F7M]Fuerte
+[Em7]Es nuestro Dios
+
+[Am]Grande
+[F7M]Fuerte
+[Em7]Es nuestro Dios
+
+[Am]Grande y [G/B]fuerte es [F7M]nuestro [Em7]Dios
+[Am]Grande y [G/B]fuerte es [F7M]nuestro [Em7]Dios
+[Am]Grande y [G/B]fuerte es [F7M]nuestro [Em7]Dios
+[Am]Grande y [G/B]fuerte es [F7M]nuestro [Em7]Dios
+
+CORO:
+[Dm7]Vestido en [G]majestad [Dm7]coronado [Am]con poder
+[F]Digno de [G]toda la [Am]adora[G/B]cion [A7/C#]
+
+[Dm7]Vestido en [G]majestad [Dm7]coronado [Am]con poder
+[F]Toda glo[G]ria y honra sea para Ti`,
+        order: 4,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-005",
+        title: "Tómalo",
+        author: "Hillsong UNITED",
+        content: `[Intro] [B] [Bbm] [D#m]
+
+[B]De todo lugar los perdidos vendrán
+[Bbm]En libertad [D#m]a Ti clamarán
+[B]Llevaste la [Bbm]cruz moriste [D#m]y vivo estás
+[G#m]Mi Dios, a [E]ti mi vida te daré
+
+[B]Enviaste a [Bbm]Jesús, por mi [D#m]salvación
+[B]Por la [Bbm]eternidad en ti [D#m]tengo perdón
+[B]Busqué la [Bbm]verdad y Te [D#m]encontré a Ti
+[G#m]Mi Dios
+[E]A Ti mi vida te daré
+
+CORO:
+[B]Jesús, por [F#]ti yo viviré
+[G#m]De ti nunca me [E]avergonzaré
+[B]Te doy todo [F#]lo que soy
+[C#m]Tóma, Tómalo
+[C#m]Tóma, Tómalo
+
+[C#m]Eres el que [G#m]vista al [F#]ciego [E]das
+[C#m]Brillas en [G#m]la oscuri[F#]dad
+[C#m]La salva[G#m]ción del [F#]mundo en [E]tus [B]manos está`,
+        order: 5,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-006",
+        title: "Tumbas a Jardines",
+        author: "Elevation Worship",
+        content: `[Intro] [B] [E/B] [B]
+
+VERSO 1:
+[E/B]El mundo [B]busqué
+[E/B]Y no pudo [B]llenarme
+[G#m]Ningún tesoro que [B/F#]pueda ganar
+[E]Me saciará
+
+VERSO 2:
+[E/B]Mas [B]llegaste tú
+[E/B]Me diste [B]vida nueva
+[G#m]Y cada deseo [B/F#]se cumplirá
+[E]Aquí en tu amor
+
+CORO:
+[B]Oh no hay nada, nada mejor
+[G#m]No hay nada, nada mejor
+[E]No hay nada, nada [B]mejor que mi Dios
+
+VERSO 3:
+[B]Vengo a ti
+[E]Sin miedo y [B]sin reservas
+[G#m]Cada fracaso has [B/F#]visto Señor
+[E]Y aún tu amigo soy
+
+VERSO 4:
+[B]Porque el Dios de los montes
+[C#m]Es el [B]Dios de los valles
+[G#m]No hay lugar que me [B/F#]pueda alejar
+[E]De tu gracia y amor
+
+PUENTE 1:
+[B]Cambias [E]lamento en [B]danza
+[E]De [B]cenizas traes vida
+[G#m]Cambias culpa por [E]gloria
+[G#m]Sé que [E]solo tú [B]lo harás
+
+PUENTE 2:
+[E]De las [B]ruinas y tumbas
+[E]Nacen [B]nuevos jardines
+[G#m]Resucitas los [E]huesos
+[G#m]Sé que [E]solo tú [B]lo harás
+[G#m]Sé que [E]solo tú [B]lo harás`,
+        order: 6,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-007",
+        title: "El poderoso de Israel",
+        author: "Juan Carlos Alvarado",
+        content: `
+[Dm]Y de noche cantaremos su poder
+[A7]Con alegría de corazón
+
+[Dm]Como el que va con la flauta
+[Dm]Al monte de Jehová celebraremos su poder
+
+CORO:
+[Dm]Él es el poderoso de Israel
+[A7]El poderoso de Israel
+
+[Dm]Su voz se oirá, nadie la detendrá
+[Dm]Al poderoso de Israel
+
+[Dm]Los ojos de los ciegos se abrirán y ellos verán
+[A7]Los oídos de los sordos oirán
+[Dm]El cojo saltará, con el arpa danzará
+[Dm]La lengua de los mudos cantará
+
+CORO:
+[Dm]Él es el poderoso de Israel
+[A7]El poderoso de Israel
+
+[Dm]Su voz se oirá, nadie la detendrá
+[Dm]Al poderoso de Israel`,
+        order: 7,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-008",
+        title: "Los muros caerán",
+        author: "Miel San Marcos",
+        content: `[Intro] [Cm] [G#] [A#] [Cm]
+
+[Cm]Cuando le canto la [A#]tierra se estremece
+[G#]Los muros [A#]ca[Cm]erán
+[Cm]Cuando le adoro se [A#]rompen las cadenas
+[G#]Los muros [A#]ca[Cm]erán
+
+CORO:
+[Cm]Los muros caerán [G#]los muros caerán
+[A#]Al sonar mi [Gm]cántico [Cm]caerán
+[Cm]Los muros caerán [G#]los muros caerán
+[A#]Con gritos de [Gm]júbilo [Cm]caerán
+
+[Cm]Cuando yo danzo [A#]aumenta Dios mis fuerzas
+[G#]Los muros [A#]ca[Cm]erán
+[Cm]Cuando yo grito mis [A#]enemigos huyen
+[G#]Los muros [A#]ca[Cm]erán
+
+PUENTE:
+[Cm]Caen los muros, [A#]caen los muros
+[F]Saltando, [Gm]saltando, los [Cm]muros [A#]caerán
+
+[Cm]Caen los muros, [A#]caen los muros
+[F]Saltando, [Gm]saltando, los [Cm]muros [A#]caerán`,
+        order: 8,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-009",
+        title: "Rey de Reyes",
+        author: "Marco Barrientos",
+        content: `[Intro] [D] [A] [Em7] [G]
+
+VERSO 1:
+[D]Rey de reyes, te [A]adoramos
+[Em7]Tu grandeza, [G]proclamamos
+[D]Rey de reyes, [A]soberano
+[Em]Tu victoria [G]celebramos
+
+PRE-CORO:
+[Bm7]Eres Jesús, el que [A]dio vida
+[Em7]A nuestro [D]corazón
+[Bm7]Eres Jesús, digno de [A]recibir
+[G]Toda [A]adoración
+
+CORO:
+[G]Rey de reyes, te [D]exaltamos
+[Bm7]Poderoso en majestad
+[A]Eres el camino y la verdad
+
+VERSO 1:
+[D]Rey de reyes, te [A]adoramos
+[Em7]Tu grandeza, [G]proclamamos
+[D]Rey de reyes, [A]soberano
+[Em]Tu victoria [G]celebramos
+
+PRE-CORO:
+[Bm7]Eres Jesús, el que [A]dio vida
+[Em7]A nuestro [D]corazón
+[Bm7]Eres Jesús, digno de [A]recibir
+[G]Toda [A]adoración
+
+CORO:
+[G]Rey de reyes, te [D]exaltamos
+[Bm7]Poderoso en majestad
+[A]Eres el camino y la verdad
+
+[G]Rey de reyes, te [D]exaltamos
+[Bm7]Poderoso en majestad
+[A]Eres el camino y la verdad
+
+PUENTE:
+[Em7]No, nadie jamás se [Bm7]podrá igualar
+[D]A tu poderío y tu majestad
+[A]Eres principio y eres final
+Y para siempre tu reinas
+
+[Em7]No, nadie jamás se [Bm7]podrá igualar
+[D]A tu poderío y tu majestad
+[A]Eres principio y eres final
+Y para siempre tu reinas
+
+CORO:
+[G]Rey de reyes, te [D]exaltamos
+[Bm7]Poderoso en majestad
+[A]Eres el camino y la verdad
+
+[G]Rey de reyes, te [D]exaltamos
+[Bm7]Poderoso en majestad
+[A]Eres el camino y la verdad
+
+PUENTE:
+[Em7]No, nadie jamás se [Bm7]podrá igualar
+[D]A tu poderío y tu majestad
+[A]Eres principio y eres final
+[G]Y para siempre tu reinas`,
+        order: 9,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-010",
+        title: "Declaro a Cristo",
+        author: "Factor de Cambio",
+        content: `VERSO 1:
+[E]Declaro el nombre de Jesús ahora
+[C#m]Sobre cada mente y corazón
+[A]Pues yo sé que hay paz en su presencia
+[E]Declaro a Cristo
+
+[E]Declaro el nombre de Jesús ahora
+[C#m]Se rompa cada vicio y adicción
+[A]Proclamo libertad y esperanza
+[E]Declaro a Cristo
+
+CORO:
+[B]Jesús tu nombre
+[A]Es poderoso
+[E]Vida nos da
+
+[B]Rompe cadenas
+[A]Vence tinieblas
+[E]Trae sanidad
+
+VERSO 2:
+[E]Declaro el nombre de Jesús ahora
+[C#m]Sobre la ansiedad y el temor
+[A]A cada ser cautivo en depresión
+[E]Declaro a Cristo
+
+CORO:
+[B]Jesús tu nombre
+[A]Es poderoso
+[E]Vida nos da
+[B]Rompe cadenas
+[A]Vence tinieblas
+[E]Trae sanidad
+
+CORO:
+[B]Jesús tu nombre
+[A]Es poderoso
+[E]Vida nos da
+[B]Rompe cadenas
+[A]Vence tinieblas
+[E]Trae sanidad
+
+PUENTE:
+[E]Tu nombre proclamamos
+Jesús en lo alto estás
+[C#m]Sobre el enemigo
+Sobre toda autoridad
+[A]Sobre mi familia
+[E]Declaro el nombre de Cristo
+
+[E]Tu nombre proclamamos
+Jesús en lo alto estás
+[C#m]Sobre el enemigo
+Sobre toda autoridad
+[A]Sobre mi familia
+[E]Declaro el nombre de Cristo
+
+[E]Tu nombre proclamamos
+Jesús en lo alto estás
+[C#m]Sobre el enemigo
+Sobre toda autoridad
+[A]Sobre mi familia
+[E]Declaro el nombre de Cristo
+
+CORO:
+[B]Jesús tu nombre
+[A]Es poderoso
+[E]Vida nos da
+[B]Rompe cadenas
+[A]Vence tinieblas
+[E]Trae sanidad`,
+        order: 10,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-011",
+        title: "Eres fiel",
+        author: "Coalo Zamorano",
+        content: `[Intro] [D] [A] [C] [G]
+
+VERSO:
+[D]Señor eres fiel y Tu [A]misericordia [C]eterna [G]
+[D]Señor eres fiel y Tu [A]misericordia [C]eterna [G]
+
+PRE-CORO:
+[G]Gente de toda [A]lengua y nación
+[G]De genera[Bb]ción, a [C]generación
+
+CORO:
+[D]Te adoramos [A]hoy, ale[C]luya, ale[G]luya
+[D]Te adoramos [A]hoy, eres [C]Señor [G]
+
+[D]Te adoramos [A]hoy, ale[C]luya, ale[G]luya
+[F#m7]Te ado[Am]ramos hoy, [A#]eres [C]Señor
+[Dm]Eres fiel
+
+PUENTE:
+[Dm]Si Se[F]ñor, si [G]Señor, si Señor
+[Dm]Tan [C]fiel, tan [G]fiel
+
+[Dm]Si Se[F]ñor, si [G]Señor, si Señor
+[Dm]Tan [C]fiel, tan [G]fiel
+
+[Dm]Eres [F]fiel, [G]siempre fiel
+[Dm]Siempre [C]fiel, [G]eres fiel
+
+[Dm]Eres [F]fiel, [G]siempre fiel
+[Dm]Siempre [C]fiel, [G]eres fiel
+
+PARTE FINAL:
+[A#]Eres Se[C]ñor, eres [A#]Señor, eres [C]Señor, eres [A#]Señor, eres [C]Señor
+[D]Eres fiel!`,
+        order: 11,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        id: "default-012",
+        title: "Vamos a Cantar",
+        author: "En Espíritu y En Verdad",
+        content: `[Intro] [E5]
+
+[E5]Vamos a cantar
+Con la música del cielo
+[C#5]Vamos a cantar
+Alegres porque escuchas
+[B5]Cuando cantamos
+[A5]Para exaltar [E5]tu nombre
+
+VERSO 1:
+[E5]Amamos todo de Ti
+Cielo y tierra Te adoran
+[C#m7]Los reinos se rinden
+[B5]Hijo de Dios, tú eres por quien
+[A5]Tú eres por quien
+[E5]Vivimos hoy
+
+[E5]Eres quien nos liberta
+Eres la luz que guía
+[C#m7]Como un fuego ardiente
+[B5]Hijo de Dios, tú eres por quien
+[A5]Tú eres por quien
+[E5]Vivimos hoy
+
+CORO:
+[E5]Vamos a cantar
+Con la música del cielo
+[C#m7]Vamos a cantar
+Alegres porque escuchas cuando
+[B5]Cantamos para [A5]exaltar [E5]tu nombre
+
+VERSO 2:
+[E5]Amamos todo de Ti
+Cielo y tierra Te adoran
+[C#m7]Los reinos se rinden
+[B5]Hijo de Dios, tú eres por quien
+[A5]Tú eres por quien
+[E5]Vivimos hoy
+
+[E5]Eres quien nos liberta
+Eres la luz que guía
+[C#m7]Como un fuego ardiente
+[B5]Hijo de Dios, tú eres por quien
+[A5]Tú eres por quien
+[E5]Vivimos hoy
+
+CORO:
+[E5]Vamos a cantar
+Con la música del cielo
+[C#m7]Vamos a cantar
+Alegres porque escuchas cuando
+[B5]Cantamos para [A5]exaltar [E5]tu nombre
+
+CORO FINAL:
+[E5]Vamos a cantar
+Con la música del cielo
+[C#m7]Vamos a cantar
+Alegres porque escuchas cuando
+[B5]Cantamos para [A5]exaltar [E5]tu nombre
+
+[E5]Vamos a cantar
+Con la música del cielo
+[C#m7]Vamos a cantar
+Alegres porque escuchas cuando
+[B5]Cantamos para [A5]exaltar [E5]tu nombre`,
+        order: 12,
         createdAt: Date.now(),
         updatedAt: Date.now()
     }
